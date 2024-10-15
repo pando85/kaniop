7b6f8c7520a7da2336c79ea5b4a9b0f6db7a53ae
Alexander Gil <pando855@gmail.com>
2024-10-15 20:32:49 +0200

    build(deps): Update kube-rs to 0.96 and tower to 0.5

--- libs/k8s-util/src/client.rs
+++ libs/k8s-util/src/client.rs
@@ -4,7 +4,7 @@ use hyper_util::rt::TokioExecutor;
 use kube::Result;
 use kube::{client::ConfigExt, Client, Config};
 use prometheus_client::registry::Registry;
-use tower::ServiceBuilder;
+use tower::{BoxError, ServiceBuilder};
 
 pub async fn new_client_with_metrics(config: Config, registry: &mut Registry) -> Result<Client> {
     let metrics_layer = MetricsLayer::new(registry);
@@ -13,6 +13,7 @@ pub async fn new_client_with_metrics(config: Config, registry: &mut Registry) ->
         .layer(metrics_layer)
         .layer(config.base_uri_layer())
         .option_layer(config.auth_layer()?)
+        .map_err(BoxError::from)
         .service(hyper_util::client::legacy::Client::builder(TokioExecutor::new()).build(https));
 
     Ok(Client::new(service, config.default_namespace))
