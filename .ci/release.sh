557d61eda50f22b8f871f70451ae4fbbc07ec5fd
Alexander Gil <pando855@gmail.com>
2025-09-18 00:09:10 +0200

    release(ci): Automatically update artifactHub annotations

--- .ci/release.sh
+++ .ci/release.sh
@@ -2,15 +2,13 @@
 set -e
 
 # bump version
-vim kaniop_core/Cargo.toml
-
-# update lock file
-cargo update -p kaniop_core
+vim Cargo.toml
+make update-version
 
 make update-changelog
 
 git add .
-VERSION=$(sed -n 's/^version = "\(.*\)"/\1/p' kaniop_core/Cargo.toml | head -n1)
+VERSION=$(sed -n 's/^version = "\(.*\)"/\1/p' Cargo.toml | head -n1)
 git commit -m "release: Version $VERSION"
 
 echo "After merging the PR, tag and release are automatically done"
